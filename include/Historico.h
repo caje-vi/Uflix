b4093a75065d128230634a661e7d2498158a26e1
Jean Carlos <jeancarlosmachado@protonmail.com>
2021-04-01 13:43:07 -0300

    historico.h

--- include/Historico.h
+++ include/Historico.h
@@ -1,9 +1,19 @@
-#infndef HISTORICO_H
+#ifndef HISTORICO_H
 #define HISTORICO_H
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include "Usuario.h"
 
 typedef struct Historico tHistorico;
+
+void AddHistorico (Usuario* Id, int IdFilme);
+
+void OrdemNota (Usuario* Id, char Tipo);
+
+
+
+
 
 #endif //USUARIO_H//
