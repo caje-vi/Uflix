b41469ea7478b2d624181dbb260f6fc819a2272b
Jean Carlos <jeancarlosmachado@protonmail.com>
2021-04-12 16:43:24 -0300

    filmes.h

--- src/Filmes.c
+++ src/Filmes.c
@@ -51,7 +51,7 @@ void ListaFilmes(tMetadados* Lista){
 
 int EntradaProximo(int n){
 
-    char opcao;
+    char opcao[100];
     int saida = 0;
     int i;
         printf("\nDigite o id do filme que deseja assitir \n");
@@ -59,17 +59,18 @@ int EntradaProximo(int n){
         printf("0 - voltar \n");
 
         while(1) {
-            scanf("%s", &opcao);
-            if(opcao == 'm' || opcao == 'M'){
+            scanf("%s", opcao);
+            if(!(strcmp("m", opcao)) || !(strcmp("M", opcao))){
                 saida = -2;
                 break;
             }
-            i = atoi(&opcao);
+            else if('a' <= opcao[1] && opcao[1] <= 'z') break;
+            i = atoi(opcao);
             if(i == 0){
                 saida = -1;
                 break;
             }
-            else if(n - 10 < i && i <= n){
+            else if(n - 9 <= i && i <= n){
                 saida =  i - 1;
                 break;
             }
@@ -96,8 +97,8 @@ tMetadados* CarregaMetadados(){
     else{
         while(fgets(aux, sizeof(aux), arquivo) != NULL){
             filme[i].titulo = strdup(strtok(aux, ","));
-            filme[i].duracao = atoi(strtok(NULL, ","));
             filme[i].ano = atoi(strtok(NULL, ","));
+            filme[i].duracao = atoi(strtok(NULL, ","));
             filme[i].nota = atoi(strtok(NULL, ","));
             filme[i].descri = strdup(strtok(NULL, "\""));
             i++;
@@ -144,7 +145,15 @@ void Avaliacao(tMetadados* Lista, int i){
     int ano;
 
     printf("O que achou de %s? De uma nota entre 0 e 10: ", Lista[i].titulo);
-    scanf("%f", &nota);
+    while(1){
+        scanf("%f", &nota);
+        if(nota < 0 && 10 < nota){
+            printf("Digite uma nota valida \n");
+        }
+        else{
+            break;
+        }
+    };
     printf("\nData: ");
     scanf("%d/%d/%d", &dia, &mes, &ano);
 };
