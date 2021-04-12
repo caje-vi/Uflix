dc18b7b8f57331e682c1547d25efe942dcc66948
Jean Carlos <jeancarlosmachado@protonmail.com>
2021-04-12 17:26:45 -0300

    Utilidades.h

--- include/Utilidades.h
+++ include/Utilidades.h
@@ -1,6 +1,10 @@
 
 int VerificaAlphaNum(const char *x);
 
+int VerificaAlpha(const char *x);
+
+int VerificaNum(const char *x);
+
 void VerificaPonteiro(void *x);
 
 void MeuPrint(char *imprime, int verbosity);
