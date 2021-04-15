6fbd1027bd0a018d79e6e2ddfc15d223fe591f20
Viniciusmj <vinijaguare@gmail.com>
2021-04-14 22:14:52 -0300

    jean troll

--- client/main.c
+++ client/main.c
@@ -8,7 +8,7 @@ int main(int argc, char *argv[]){
     int verbosity, *tamUser, *userAtivos;
     tMetadados* filmes;
     tUsuario *users;
-
+    
     if(argc == 2){
         verbosity = atoi(argv[1]);
     }
